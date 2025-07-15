a445486ceda234a8d13bc105d4f2291bdc856204
Gökay Meydan <meygokay@icloud.com>
2025-07-15 20:34:54 +0200

    Some fixes

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import matplotlib.pyplot as plt
 from datetime import datetime, date, time
 from src.modelling import load_model
