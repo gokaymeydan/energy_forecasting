27003217f3d83e8b678107fd32a746f8ce7a5165
Gökay Meydan <meygokay@icloud.com>
2025-07-26 23:59:23 +0200

    some updates

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 import os
-import sys
 from datetime import date, datetime, time
 
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import streamlit as st
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 from src.modelling import load_model
-from src.preprocessing import load_and_preprocess
-
-os.makedirs("outputs", exist_ok=True)
 
 st.set_page_config(page_title="Energy Forecasting App", layout="wide")
 st.title("Energy Consumption Forecasting App")
@@ -34,7 +27,7 @@ selected_time = st.sidebar.time_input("Select an hour", time(14, 0))
 dt = datetime.combine(selected_date, selected_time)
 
 st.info(
-    "Note: The predictions are based on a house located in Sceaux (7km of Paris, France) between December 2006 and November 2010 (47 months)"
+    "The predictions are based on a house located in Sceaux (7km of Paris, France) between December 2006 and November 2010."
 )
 
 future_dates = pd.date_range(start=dt, periods=168, freq="h")
@@ -49,20 +42,10 @@ if st.sidebar.button("Predict"):
     predictions = model.predict(X)
     future_df["Prediction"] = predictions
     st.success("Prediction completed successfully.")
-    future_df.to_csv("outputs/output.csv", index=False)
 else:
     future_df = pd.DataFrame()
 
 if not future_df.empty:
-    plt.figure(figsize=(10, 5))
-    plt.plot(future_df["Datetime"], future_df["Prediction"], label="Predicted")
-    plt.legend()
-    plt.title("Predicted Power Usage for Selected Week")
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.savefig("outputs/prediction_plot.png")
-    plt.close()
-
     st.subheader("Predicted Power Usage (Next 7 days)")
     st.line_chart(
         future_df.set_index("Datetime")["Prediction"].rename("Predicted Power (kW)")
